3ce8bcd6b86a27c2be6ada169299b087262446dc
tracer-ysyx <tracer@ysyx.org>
2024-03-27 19:40:17 +0800

    > sim RTL ysyx_22040001 Wang BoTao Linux ubuntu-linux-22-04-02-desktop 5.15.0-101-generic #111-Ubuntu SMP Wed Mar 6 18:01:01 UTC 2024 aarch64 aarch64 aarch64 GNU/Linux  19:40:17 up  1:54,  1 user,  load average: 0.30, 0.16, 0.11

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-STUID = ysyx_22040000
-STUNAME = 张三
+STUID = ysyx_22040001
+STUNAME = Wang BoTao
 
 # DO NOT modify the following code!!!
 
--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,27 @@
-#include <stdio.h>
+#include "Vtop.h"      // 包含由 Verilator 生成的头文件
+#include "verilated.h" // Verilator 的基础库
+#include "verilated_vcd_c.h" // 波形输出的库
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+int main(int argc, char **argv) {
+    Verilated::commandArgs(argc, argv); // 处理 Verilator 的参数
+    Vtop* top = new Vtop; // 创建一个 Verilog 模块实例
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* vcd = new VerilatedVcdC;
+    top->trace(vcd, 5); // 设置追踪深度
+    vcd->open("waveform.vcd"); // 创建 VCD 文件
+
+    for (int i = 0; i < 10; i++) {
+        top->a = i & 1; // 改变输入
+        top->b = (i >> 1) & 1;
+        top->eval(); // 评估 Verilog 模块
+
+        vcd->dump(i); // 记录波形
+    }
+
+    vcd->close(); // 关闭 VCD 文件
+    delete top; // 清理实例
+    delete vcd; // 清理波形文件对象
+    exit(0);
 }
+
