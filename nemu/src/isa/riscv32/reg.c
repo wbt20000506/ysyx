221e26f5915569d6a6fe6a45e6a654eb71468074
tracer-ysyx <tracer@ysyx.org>
2024-04-11 19:55:20 +0800

    > compile NEMU ysyx_22040001 Wang BoTao Linux ubuntu-linux-22-04-02-desktop 5.15.0-101-generic #111-Ubuntu SMP Wed Mar 6 18:01:01 UTC 2024 aarch64 aarch64 aarch64 GNU/Linux  19:55:20 up  4:54,  1 user,  load average: 1.02, 0.62, 0.45

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,6 +24,16 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+  int count=0;
+  for (int i = 0; i < 32; i++){
+    printf("%s=0x%08x\t",reg_name(i),cpu.gpr[i]);
+    count++;
+    if (count==3 || i==31)
+    {
+      printf("\n");
+      count=0;
+    }  
+  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
