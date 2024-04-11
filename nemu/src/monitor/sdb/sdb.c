01524e796cdaca8189f449b5defee39e6b9f4911
tracer-ysyx <tracer@ysyx.org>
2024-04-11 16:24:12 +0800

    > compile NEMU ysyx_22040001 Wang BoTao Linux ubuntu-linux-22-04-02-desktop 5.15.0-101-generic #111-Ubuntu SMP Wed Mar 6 18:01:01 UTC 2024 aarch64 aarch64 aarch64 GNU/Linux  16:24:12 up  2:26,  1 user,  load average: 0.51, 0.35, 0.34

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -52,6 +52,20 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+
+static int cmd_si(char *args){
+  char *arg = strtok(NULL, " ");
+  if (arg==NULL)
+  cpu_exec(1);
+  else{
+    int i;
+    sscanf(arg,"%d",&i);
+    cpu_exec(i);
+  }
+  return 0;
+}
+
+
 static int cmd_help(char *args);
 
 static struct {
@@ -62,7 +76,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si", "Step", cmd_si },
   /* TODO: Add more commands */
 
 };
