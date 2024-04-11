db1c9aa15d37a1763cdb5cf0cca75bb85b3b63ca
tracer-ysyx <tracer@ysyx.org>
2024-04-11 17:32:53 +0800

    > compile NEMU ysyx_22040001 Wang BoTao Linux ubuntu-linux-22-04-02-desktop 5.15.0-101-generic #111-Ubuntu SMP Wed Mar 6 18:01:01 UTC 2024 aarch64 aarch64 aarch64 GNU/Linux  17:32:53 up  3:34,  1 user,  load average: 0.30, 0.23, 0.27

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -35,11 +35,7 @@ run-env: $(BINARY) $(DIFF_REF_SO)
 
 run: run-env
 	$(call git_commit, "run NEMU")
-ifeq ($(strip $(IMG)),)
-	$(BINARY) $(ARGS)
-else
-	$(BINARY) $(ARGS) $(IMG)
-endif
+	$(NEMU_EXEC)
 
 gdb: run-env
 	$(call git_commit, "gdb NEMU")
