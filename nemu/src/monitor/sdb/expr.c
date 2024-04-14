ad4c6d5796029bf8055faaafcc71fe7bb589422a
tracer-ysyx <tracer@ysyx.org>
2024-04-14 21:33:01 +0800

    > compile NEMU ysyx_22040001 Wang BoTao Linux ubuntu-linux-22-04-02-desktop 5.15.0-102-generic #112-Ubuntu SMP Tue Mar 5 16:49:56 UTC 2024 aarch64 aarch64 aarch64 GNU/Linux  21:33:01 up  6:00,  1 user,  load average: 0.10, 0.19, 0.25

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -107,7 +107,7 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-
+        
         switch (rules[i].token_type) {
           case TK_NOTYPE:{if (nr_token==0)break; else {nr_token--; break;}}
           case '*':tokens[nr_token].type = TK_MUL;break;
