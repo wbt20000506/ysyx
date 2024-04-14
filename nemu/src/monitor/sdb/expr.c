bcb319e1e37aef9a1220e30bea292c54f4cece56
tracer-ysyx <tracer@ysyx.org>
2024-04-14 21:35:16 +0800

    > compile NEMU ysyx_22040001 Wang BoTao Linux ubuntu-linux-22-04-02-desktop 5.15.0-102-generic #112-Ubuntu SMP Tue Mar 5 16:49:56 UTC 2024 aarch64 aarch64 aarch64 GNU/Linux  21:35:16 up  6:02,  1 user,  load average: 1.01, 0.43, 0.32

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -148,7 +148,7 @@ word_t expr(char *e, bool *success) {
 }
 
 word_t eval(int p,int q,bool *success){
-  if(tokens[p].type==TK_NOTYPE)
+  if(tokens[p].type==0)
   q--;
   Log("nr_token:%d",q);
   *success=true;
