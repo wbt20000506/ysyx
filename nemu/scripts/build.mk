a590df095d905ba04d82f616914b0b1b6ceae304
tracer-ysyx <tracer@ysyx.org>
2024-04-14 16:31:25 +0800

    > compile NEMU ysyx_22040001 Wang BoTao Linux ubuntu-linux-22-04-02-desktop 5.15.0-102-generic #112-Ubuntu SMP Tue Mar 5 16:49:56 UTC 2024 aarch64 aarch64 aarch64 GNU/Linux  16:31:25 up  2:05,  1 user,  load average: 0.52, 0.40, 0.34

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -37,7 +37,7 @@ $(OBJ_DIR)/%.o: %.c
 $(OBJ_DIR)/%.o: %.cc
 	@echo + CXX $<
 	@mkdir -p $(dir $@)
-	@$(CXX) $(CFLAGS) $(CXXFLAGS) -c -o $@ $<
+	@$(CXX) -g $(CFLAGS) $(CXXFLAGS) -c -o $@ $<
 	$(call call_fixdep, $(@:.o=.d), $@)
 
 # Depencies
