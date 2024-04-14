ab823636345c09bfd4de9556b692c16f210b50a1
tracer-ysyx <tracer@ysyx.org>
2024-04-14 21:04:31 +0800

    > compile NEMU ysyx_22040001 Wang BoTao Linux ubuntu-linux-22-04-02-desktop 5.15.0-102-generic #112-Ubuntu SMP Tue Mar 5 16:49:56 UTC 2024 aarch64 aarch64 aarch64 GNU/Linux  21:04:31 up  5:31,  1 user,  load average: 0.75, 0.56, 0.57

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -30,56 +30,35 @@ static char *code_format =
 "  printf(\"%%u\", result); "
 "  return 0; "
 "}";
-static char *buf_start = NULL;
-static char *buf_end = buf+(sizeof(buf)/sizeof(buf[0]));
-
-static int choose(int n) {
-  return rand() % n;
+static int index_buf;
+int choose(int n)
+{
+	return rand() % n;
 }
 
-static void gen_space() {
-  int size = choose(4);
-  if (buf_start < buf_end) {
-    int n_writes = snprintf(buf_start, buf_end-buf_start, "%*s", size, "");
-    if (n_writes > 0) {
-      buf_start += n_writes;
+void gen_num(){
+    buf[index_buf++]='0'+rand()%10;
+}
+void gen_rand_op(){
+    int i=rand()%4;
+    switch (i)
+    {
+    case 0:buf[index_buf++]='+';break;
+    case 1:buf[index_buf++]='-';break;
+    case 2:buf[index_buf++]='*';break;
+    case 3:buf[index_buf++]='/';break;
     }
-  }
+}
+void gen(char a){
+    buf[index_buf++]=a;
 }
 
-static void gen_num() {
-  int num = choose(INT8_MAX);
-  if (buf_start < buf_end) {
-    int n_writes = snprintf(buf_start, buf_end-buf_start, "%d", num);
-    if (n_writes > 0) {
-      buf_start += n_writes;
-    }
-  }
-  gen_space();
-}
 
-static void gen_char(char c) {
-  int n_writes = snprintf(buf_start, buf_end-buf_start, "%c", c);
-  if (buf_start < buf_end) {
-    if (n_writes > 0) {
-      buf_start += n_writes;
-    }
-  }
-}
-
-static char ops[] = {'+', '-', '*', '/'};
-static void gen_rand_op() {
-  int op_index = choose(sizeof(ops));
-  char op = ops[op_index];
-  gen_char(op);
-}
-
-static void gen_rand_expr() {
-  switch (choose(3))
-  {
-  case 0: gen_num(); break;
-  case 1: gen_char('('); gen_rand_expr(); gen_char(')'); break;
-  default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+void  gen_rand_expr() {
+  switch (choose(3)) {
+    case 0: gen_num(); break;
+    case 1: gen('('); gen_rand_expr(); gen(')'); break;
+    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
   }
 }
 
@@ -92,11 +71,7 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
-    buf_start = buf;
-    
     gen_rand_expr();
-    
-
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
@@ -104,19 +79,19 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc /tmp/.code.c -Wall -Werror -o /tmp/.expr");
-    // filter div-by-zero expressions
+    int ret = system("gcc -Wall -Werror /tmp/.code.c -o /tmp/.expr");
     if (ret != 0) continue;
 
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
 
-    uint64_t result;
-    int _ = fscanf(fp, "%lu", &result);
-    _ = _;
+    int result;
+    ret = fscanf(fp, "%d", &result);
     pclose(fp);
-    
-    printf("%lu %s\n", result, buf);
+
+    printf("%u %s\n", result, buf);
+    index_buf=0;
+    buf[0]='\0';
   }
   return 0;
 }
